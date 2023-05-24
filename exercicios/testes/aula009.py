14b17fcbe95fd721f776e3c96a16d99f3a766635
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-05-24 18:48:31 -0300

    aula 9

---
+++ exercicios/testes/aula009.py
@@ -0,0 +1,15 @@
+frase = 'curso em video python'
+# print(frase.upper().count('O'))  -> conta quantos O na frase em maiuscula
+# print(len(frase))  -> comprimento da frase
+# print(frase.replace('python', 'carro')) -> 
+print(frase.split())
+
+
+
+
+
+
+###print(""" teste  # 3 aspas duplas no começo e no final para varias linhas 
+#teste
+#testando
+#""")
