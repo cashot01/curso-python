344a390b72f9b5b90a2943f6acb18334e95a31b2
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-05-24 16:20:15 -0300

    exercicio 20

--- exercicios/ex020.py
+++ exercicios/ex020.py
@@ -1,6 +1,9 @@
 import random 
 n1 = str(input('aluno 1: '))
 n2 = str(input('aluno 2: '))
-n3 = (input('aluno 3: '))
-n4 = (input('aluno 4: '))
+n3 = str(input('aluno 3: '))
+n4 = str(input('aluno 4: '))
 lista = [n1, n2, n3, n4]
+random.shuffle(lista)    # shuffle = embaralhar
+print('ordem de apresentação: ')
+print(lista)
