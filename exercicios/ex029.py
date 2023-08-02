d2e1c25e8eee08363ee365263b9a5e5e57bf1a70
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-02 16:15:29 -0300

    29 e 30

---
+++ exercicios/ex029.py
@@ -0,0 +1,7 @@
+# radar com multa > 80km/h multa de R$7,00
+v = int(input('velocidade: '))
+multa = v*7
+if v > 80:
+    print('vc foi multado, vai custar R${:.2f}'.format(multa))
+else:
+    print('velocidade aceita')    
---
+++ exercicios/ex030.py
@@ -0,0 +1,6 @@
+# numero inteiro e mostre se é par ou impar
+n = int(input('digite numero inteiro: '))
+if n % 2 == 0: 
+    print('par')
+else:
+    print('impar')    
