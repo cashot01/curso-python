8538b60d04115a982a4f7c5138a343c3edd4a3fa
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-03 16:17:48 -0300

    33

--- exercicios/ex033.py
+++ exercicios/ex033.py
@@ -2,3 +2,19 @@
 n1 = int(input('n1: '))
 n2 = int(input('n2: '))
 n3 = int(input('n3: '))
+#verificando o menor
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3 
+print('menor nº: {}'.format(menor))    
+# verificando o maior
+maior = n1
+if n2 > n1 and n2 > n3 :
+    maior = n2
+if n3 > n1 and   n3 > n2:
+    maior = n3 
+print('maior nº: {}'.format(maior)) 
+diferenca = maior - menor
+print('a diferença entre o maior {} e o menor {} é = {}'.format(maior, menor, diferenca))   
