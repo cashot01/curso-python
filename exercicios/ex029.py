b85adac371061275c3a297f947fce9a328b73f8c
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-03 15:16:53 -0300

    29

--- exercicios/ex029.py
+++ exercicios/ex029.py
@@ -1,6 +1,6 @@
 # radar com multa > 80km/h multa de R$7,00
 v = int(input('velocidade: '))
-multa = v*7
+multa = (v-80)*7
 if v > 80:
     print('vc foi multado, vai custar R${:.2f}'.format(multa))
 else:
