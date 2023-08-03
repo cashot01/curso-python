9a919e6f4a11da1dff4d5590a312f1fdc76287a0
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-03 15:52:28 -0300

    32

--- exercicios/ex032.py
+++ exercicios/ex032.py
@@ -1,6 +1,11 @@
+from datetime import date
 # leia ano , mostre se é bissexto
+
 ano = int(input('digite um ano: '))
-if ano % 4 == 0:
+if ano == 0:
+    ano = date.today().year
+print(ano)
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('ano bissexto')
 else:
     print('ano normal')    
