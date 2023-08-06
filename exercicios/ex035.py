d037772942b6d9adbe220d340a11773a36012636
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-06 14:44:53 -0300

    35

--- exercicios/ex035.py
+++ exercicios/ex035.py
@@ -1,1 +1,8 @@
 # Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+r1 = float(input('reta 1: '))
+r2 = float(input('reta 2: '))
+r3 = float(input('reta 3: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('as 3 retas formam um triangulo')
+else:
+    print('não forma um triangulo')
