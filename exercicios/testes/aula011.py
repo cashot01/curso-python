1fc136ebf55e342c32ba1f0fa49a7c1f93d7f821
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-07 17:37:19 -0300

    aula11

---
+++ exercicios/testes/aula011.py
@@ -0,0 +1,8 @@
+# cores no terminal
+'''print('\033[0;31mTESTE')
+print('\033[1;32mCauan\033[m')
+print('\033[4;33mOlá')   '''
+
+'''nome = 'Cashot01'
+print('beleza, prazer em conhecer {}{}{}'.format('\033[4;34m', nome, '\033[m')) '''
+# 1º e ultimos {} cor de inicio e do fim
