c401ee21d2c9c8891cd77077328661a5dffb35ab
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-07 17:45:11 -0300

    ex7

--- exercicios/ex007.py
+++ exercicios/ex007.py
@@ -4,7 +4,7 @@ n2 = float(input('nota 2: '))
 media = (n1+n2)/2
 print('sua nota 1 {}, sua nota 2 {}, seu media {}'.format(n1, n2, media))
 if media < 6:
-    print('vc foi reprovado')
+    print('\033[0;31mvc foi reprovado\033[m')
 else:
-    print('vc foi aprovado');
+    print('\033[1;32mvc foi aprovado\033[m');
 
