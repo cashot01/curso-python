c4189f120eb99d1b8e5cc33b75094b6ce11d25fd
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-09 15:34:52 -0300

    aula 12

---
+++ exercicios/testes/aula012.py
@@ -0,0 +1,7 @@
+nome = str(input('seu nome: '))
+if nome == 'Cauan':
+    print('que name encantador')
+elif nome == 'pedro' or nome == 'gabriel' or nome == 'julia':
+    print('nome popular no Brasil')
+elif nome in 'ana claudia rafaela gabriela':
+    print('nome feminino')    
