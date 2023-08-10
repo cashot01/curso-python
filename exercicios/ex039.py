30d9a8551445c366dba4b0d730f38ea4ca6ecf89
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-10 15:39:28 -0300

    39

--- exercicios/ex039.py
+++ exercicios/ex039.py
@@ -1,1 +1,15 @@
 # idade , mostre se ainda vai se alistar, ano de alist5amento, ja passou o alistamento, mostrar 
+anoAtual = int(input('ano atual: '))
+nascimento = int(input('ano de nascimento: '))
+idade = anoAtual - nascimento
+alistamento = 18
+menos18 = alistamento - idade
+mais18 = idade - alistamento
+print('idade: {} anos'.format(idade))
+if idade == 18:
+    print('alistamento nesse ano, fique esperto')
+elif idade < 18:
+    print('seu alistamento acontece em: {} anos'.format(menos18))
+elif idade > 18:
+    print('alistamento feito há {} anos atras'.format(mais18))    
+
