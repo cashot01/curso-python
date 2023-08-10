900d9b3c52f7466bfa4381908715eee932ed8186
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-10 16:05:09 -0300

    41

---
+++ exercicios/ex041.py
@@ -0,0 +1,15 @@
+# mostre idade, atleta ate 9 mirim, 14 infantil, 19 junior, 20 senior, >20 master
+anoAtual = int(input('ano atual: ')) 
+nascimento = int(input('ano nascimento: '))
+idade = anoAtual - nascimento
+print('idade: {} anos'.format(idade))
+if idade <= 9:
+    print('nadador MIRIM')
+elif 9 < idade <= 14:
+    print('nadador INFANTIL')    
+elif  14 < idade <= 19:
+    print('nadador JUNIOR') 
+elif idade == 20:
+    print('nadador SENIOR')       
+elif idade > 20:
+    print('nadador MASTER')    
