bd9af4b0e4b1ae46eb847fd32b608ef79166ee23
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-17 14:59:26 -0300

    44

--- exercicios/ex044.py
+++ exercicios/ex044.py
@@ -1,16 +1,21 @@
 # calcule o valaor do produto, a vista (dinheiro/ cheque) 10% desconto, a vista (cartao) 5% desconto, 2x no cartao preço normal, 3x cartao 20% de juros
 valProduto = float(input('valor do produto: R$'))
-pagamento = str(input('forma de pagamento: '))
+print(''' Formas de pagamento
+      [1] dinheiro/cheque
+      [2] cartão
+      [3] 2x no cartão
+      [4] 3x ou mais no cartao ''')
+pagamento = int(input('forma de pagamento: '))
 dinheiroCheque =  valProduto -(valProduto * 0.10)
 cartao = valProduto - (valProduto * 0.05)
 juros = valProduto + (valProduto * 0.20)
-if pagamento == 'dinheiro' or pagamento == 'cheque':
-    print('vc pagou em {} e tem desconto de 10%, novo valor:  R${:.2f}'.format(pagamento, dinheiroCheque))
-elif pagamento == 'cartao':
+if pagamento == 1:
+    print('vc pagou no dinheiro/cheque  e tem desconto de 10%, novo valor:  R${:.2f}'.forma(dinheiroCheque))
+elif pagamento == 2:
+        print('pagamento no cartão , desconto de 5%, novo preço: R${:.2f}'.format(cartao)) 
+          
+elif pagamento == 3:       
+    print('preço normal, R${}'.format(valProduto))    
+elif pagamento == 4 :
     parcelado = int(input('quantas vezes gostaria de parcelar? '))
-    if parcelado == 1:
-        print('pagamento no {} , desconto de 5%, novo preço: R${:.2f}'.format(pagamento, cartao))  
-    elif parcelado == 2:          
-        print('preço normal, R${}'.format(valProduto))    
-    elif parcelado >= 3 :
-        print('parcelado em {}, juros de 20% , valor: R${}'.format(pagamento, juros))    
+    print('parcelado em {}x, juros de 20% , valor: R${}'.format(parcelado, juros))    
