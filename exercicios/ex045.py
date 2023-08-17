f966d302b935a1d69bbdfa51576161851cd90d3c
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-17 15:25:44 -0300

    45

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -1,3 +1,23 @@
 # pedra papel tesousa
 import random
+print(''' JOKENPÔ
+      [1] pedra
+      [2] papel
+      [3] tesoura ''')
+maquina = random.randint(1, 3)
+myJogada = int(input('sua escolha: '))
+if maquina == myJogada:
+    print('EMPATE')
+if maquina == 1 and myJogada == 3:
+    print('PC ganhou, pedra > tesoura ')  
+elif maquina == 2 and myJogada == 1:
+    print('PARABENS vc GANHOU, papel > pedra')    
+elif maquina == 3 and myJogada ==  2:
+    print('PC ganhou, tesoura > papel')
+elif myJogada == 1 and maquina == 3:
+    print('PARABENS vc ganhou, pedra > tesoura ')    
+elif myJogada == 2 and maquina == 1:
+    print('PARABENS ganhou, papel > pedra')  
+elif myJogada == 3 and maquina == 2:
+    print('PARABENS vc ganou, tesoura > papel')      
 
