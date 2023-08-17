704b37febaaa65d6834e838a561b1b0c9641c7dc
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-17 14:34:11 -0300

    42

--- exercicios/ex042.py
+++ exercicios/ex042.py
@@ -3,7 +3,7 @@ r1 = float(input('reta 1: '))
 r2 = float(input('reta 2: '))
 r3 = float(input('reta 3: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 +r2:
-    print('forma um triangulo')
+    print('forma um triangulo')    
 elif r1 > r2 + r3 and r2 > r1 + r3 and r3 > r1 +r2:
     print('nao forma triangulo')
 if r1 == r2 and r1 == r3 and r2 == r3:   
