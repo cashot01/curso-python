f183c1acd3d9a0356e6fefb9d0e2e30ec394570d
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2023-08-23 15:32:03 -0300

    aula 13

--- exercicios/testes/aula013.py
+++ exercicios/testes/aula013.py
@@ -1,3 +1,13 @@
-for c in range(1, 6):
+'''i = int(input('inicio: '))
+fim = int(input('fim: '))
+passo = int(input('passo: '))
+for c in range(i, fim+1, passo):  
+     # range(intervalo é os 2 primeiros , o terceiro é a iteração(nesse exemplo quantos pulos vai dar)) range(0, 10, 2(vai pular de 2 em 2) ) 
+     # no 2º nº do range sempre vai ser o anterior, para adicionar basta colocar +1
     print(c)
-print('fim')    
+print('FINALIZOU')   '''
+s = 0
+for c in range(0, 4):
+    n = int(input('digite algum nº: '))
+    s = s + n   # ou s += n
+print('a soma de todos os nº foi de {}'.format(s))    
