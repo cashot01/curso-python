689eb832b31ebd0ff977fcdb104787eed71fc788
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-01-23 16:21:58 -0300

    52

--- exercicios/ex052.py
+++ exercicios/ex052.py
@@ -1,5 +1,16 @@
 # leia nº inteiro e fala se é numero primo ou não
-for c in range(2, n):
-   if c / c == 1 and c / 1 == c:
-     print(c)
+num = int(input('nº: '))
+tot = 0
+for c in range(1, num +1):
+   if num % c == 0:
+     print('\033[32m', end='')
+     tot += 1
+   else:
+      print('\033[31m', end='')
+   print('{} '.format(c), end='')
+print('\n\033[m0 numero {} foi divisivel {} vezes'.format(num, tot))
+if tot == 2:
+   print('nº primo')
+else:
+   print('NÃO PRIMO')
  
